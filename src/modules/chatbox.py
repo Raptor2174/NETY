728b8f0be6ddcbb6cb6046c3742a1266554661f5
Raptor2174 <108515279+Raptor2174@users.noreply.github.com>
2023-10-03 01:17:53 +0200

    chatbox + cpython-311 ?

--- src/modules/chatbox.py
+++ src/modules/chatbox.py
@@ -1,12 +1,15 @@
-from src.modules.module_text.modele_rnn import ModeleRNN
-from src.modules.module_image.modele_cnn import ModeleCNN
-from src.modules.module_audio.module_stt import ModeleSTT
+from module_text.modele_rnn import ModeleRNN
+from module_image.modele_cnn import ModeleCNN
+from module_audio.module_stt import ModeleSTT
 from preprocessing.text_preprocessing import preprocess_input
 from module_image.modele_cnn import ModeleCNN
 from module_audio.module_stt import ModeleSTT
 from postprocessing.text_postprocessing import postprocess_output
-
-
+from PIL import Image
+from preprocessing.image_preprocessing import preprocess_image
+from preprocessing.image_preprocessing import preprocess_input
+from preprocessing.audio_preprocessing import preprocess_audio
+import torchaudio
 
 def generer_reponse(entree_utilisateur, modele_texte, modele_image, modele_audio):
     # Prétraitez l'entrée utilisateur si nécessaire
@@ -16,10 +19,14 @@ def generer_reponse(entree_utilisateur, modele_texte, modele_image, modele_audio
     predictions_texte = modele_texte(donnees_pretraitees)
 
     # Utilisez le modèle de vision par ordinateur pour les prédictions d'images (si applicable)
-    predictions_images = modele_image(images)
+    image = Image.open("data/images/image1.jpg")
+    image = preprocess_image(image)
+    predictions_images = modele_image(image)
 
     # Utilisez le modèle de reconnaissance vocale pour la conversion audio en texte (si applicable)
-    predictions_audio = modele_audio(audio)
+    waveform, sample_rate = torchaudio.load("data/audio/audio_data.wav") 
+    waveform = preprocess_audio(waveform)
+    predictions_audio = modele_audio(waveform)
 
     # Post-traitez les prédictions et générez une réponse
     reponse_generale = postprocess_output(predictions_texte, predictions_images, predictions_audio)
