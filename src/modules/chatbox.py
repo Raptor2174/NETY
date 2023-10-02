84c2128ed823c0f2810459a8e3582011b9e73f04
Raptor2174 <108515279+Raptor2174@users.noreply.github.com>
2023-10-03 00:03:05 +0200

    Update chatbox.py

--- src/modules/chatbox.py
+++ src/modules/chatbox.py
@@ -1,21 +1,21 @@
-from modele_rnn import modele_rnn
-from modele_cnn import modele_cnn
-from stt_module import modele_stt
+from src.modules.module_text.modele_rnn import ModeleRNN
+from src.modules.module_image.modele_cnn import ModeleCNN
+from src.modules.module_audio.module_stt import ModeleSTT
 
-def generer_reponse(entree_utilisateur):
+def generer_reponse(entree_utilisateur, modele_texte, modele_image, modele_audio):
     # Prétraitez l'entrée utilisateur si nécessaire
     donnees_pretraitees = preprocess_input(entree_utilisateur)
 
-    # Utilisez le modèle RNN pour les prédictions de texte
-    predictions_texte = modele_rnn(donnees_pretraitees)
+    # Utilisez le modèle de traitement de texte pour les prédictions
+    predictions_texte = modele_texte(donnees_pretraitees)
 
-    # Utilisez le modèle CNN pour les prédictions d'images (si applicable)
-    predictions_images = modele_cnn(images)
+    # Utilisez le modèle de vision par ordinateur pour les prédictions d'images (si applicable)
+    predictions_images = modele_image(images)
 
-    # Utilisez le modèle STT pour la reconnaissance vocale (si applicable)
-    predictions_audio = modele_stt(audio)
+    # Utilisez le modèle de reconnaissance vocale pour la conversion audio en texte (si applicable)
+    predictions_audio = modele_audio(audio)
 
     # Post-traitez les prédictions et générez une réponse
-    reponse_generalee = postprocess_output(predictions_texte, predictions_images, predictions_audio)
+    reponse_generale = postprocess_output(predictions_texte, predictions_images, predictions_audio)
 
-    return reponse_generalee
+    return reponse_generale
