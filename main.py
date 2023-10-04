21c384aae694b124cd280a9aae3c217faf5eb5be
Raptor2174 <108515279+Raptor2174@users.noreply.github.com>
2023-10-04 20:10:37 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,25 +1,32 @@
+import tkinter as tk
 from src.modules.module_text.modele_rnn import ModeleRNN
 from src.modules.module_image.modele_cnn import ModeleCNN
 from src.modules.module_audio.module_stt import ModeleSTT
 from src.modules.chatbox import generer_reponse
 
-def main():
-    # Créez des instances des modèles de texte, d'image et d'audio
-    modele_texte = ModeleRNN(input_size=..., hidden_size=..., output_size=...)
-    modele_image = ModeleCNN(...)
-    modele_audio = ModeleSTT(...)
+def poser_question():
+    entree_utilisateur = entree.get()
+    reponse = generer_reponse(entree_utilisateur, modele_texte, modele_image, modele_audio)
+    reponse_label.config(text="NETY: " + reponse)
 
-    # Boucle principale
-    while True:
-        # Lisez l'entrée de l'utilisateur
-        entree_utilisateur = input("Vous: ")
+# Créez des instances des modèles de texte, d'image et d'audio
+modele_texte = ModeleRNN(input_size=..., hidden_size=..., output_size=...)
+modele_image = ModeleCNN(...)
+modele_audio = ModeleSTT(...)
 
-        # Utilisez le chatbox pour générer une réponse
-        reponse = generer_reponse(entree_utilisateur, modele_texte, modele_image, modele_audio)
+# Créez la fenêtre Tkinter
+fenetre = tk.Tk()
+fenetre.title("NETY IA")
 
-        # Affichez la réponse
-        print("NETY: ", reponse)
+# Ajoutez un champ d'entrée et un bouton
+entree = tk.Entry(fenetre)
+entree.pack()
+bouton = tk.Button(fenetre, text="Poser une question", command=poser_question)
+bouton.pack()
 
-if __name__ == "__main__":
-    main()
-    
+# Ajoutez une zone de texte pour afficher la réponse
+reponse_label = tk.Label(fenetre, text="")
+reponse_label.pack()
+
+# Lancez la boucle principale de Tkinter
+fenetre.mainloop()
